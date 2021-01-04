e90fb20e572342282dbfcd0bbd9679d27c2524af
Dionatan Korb <dmkorb@gmail.com>
2021-01-04 09:36:57 -0300

    Moving event handlers to events dir

--- server/src/services/stocks.service.js
+++ server/src/services/stocks.service.js
@@ -72,28 +72,3 @@ export {
   getBenchmarks
 }
 
-
-/**
- * Event handlers
- */
-const onTradeCreated = async (data) => {
-  try {
-    const { trade } = data;
-    if (!trade || !trade.symbol || !trade.portfolio_id) {
-      throw new Error(`Trade created event with invalid trade object: ${JSON.stringify(trade)}`);
-    }
-
-    logger.info(`Stocks onTradeCreated handler - trade ID ${trade.id} for ${trade.symbol}`);
-
-    // Get time series when a trade is created. Even if not used here, it'll be cached in Redis for later use.
-    const timeSeries = await getTimeSeries(trade.symbol)
-    logger.info('onTradeCreated - timeseries length', timeSeries?.length);
-  } catch (err) {
-
-  }
-}
-
-/**
- * Event listners
- */
-em.on(EVENTS.TRADE.TRADE_CREATED, onTradeCreated)
